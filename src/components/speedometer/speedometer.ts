70b445f1b01f47169934ba01702c00724eaa8717
Fellype Souza <fellypedev23@gmail.com>
2025-06-18 15:56:40 -0300

    adicionando a função do velocimetro

--- src/components/speedometer/speedometer.ts
+++ src/components/speedometer/speedometer.ts
@@ -10,11 +10,46 @@ export class Speedometer {
   speedElement:number = 0;
   unitSpeed:string = "KM/H";
   changeButton:boolean = true;
+  watchId:any = null;
+  maxSpeed:number = 0;
+  latitudeValue:number = 0;
+  longitudeValue:number = 0;
 
   startSpeedometer():void{
+    //Mudar o estado do botão
     this.changeButton = false;
+
+    //Começo a capturar a localização
+    this.watchId = navigator.geolocation.watchPosition((position)=>{
+        this.latitudeValue = position.coords.latitude;
+        this.longitudeValue = position.coords.longitude;
+
+        //Verificação se houve mudança na velocidade
+        if (position.coords.speed != null) {
+          //Recebo o valor da velocidade, multiplico por 3.6 para converter de m/s para km/h, e ajusto para apenas uma casa decimal
+          this.speedElement = parseInt((position.coords.speed * 3.6).toFixed(1));
+          //Verificar se a velocidade atual é maior que a Velocidade Maxima
+          if (this.speedElement > this.maxSpeed) {
+            this.maxSpeed = this.speedElement;
+          }
+        }
+        else{
+          this.speedElement = 0;
+        }
+      },
+      (error)=>{
+        alert("Houve um erro");
+        console.log(`Erro: ${error}`);
+      }, 
+      {
+        enableHighAccuracy: true
+      }
+    )
   }
   stopSpeedometer():void{
+    //Mudo o estado do botão
     this.changeButton = true;
+    //Paro de capturar a localização
+    navigator.geolocation.clearWatch(this.watchId);
   }
 }
